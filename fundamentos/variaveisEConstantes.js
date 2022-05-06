335a28fcd82c316ff185bdf6f5e4f9b8f1d4f224
Kevyn Lima <kevynlimasilva1@gmail.com>
2022-05-05 21:42:13 -0300

    1.0.0.0.0

--- fundamentos/variaveisEConstantes.js
+++ fundamentos/variaveisEConstantes.js
@@ -1,7 +1,7 @@
-var a = 3;
+/*var a = 3;
 let b = 4;
 /* crie variaveis utilizando let é maneira mais utilizada */
-
+/*
 var a = 30;
 b = 40;
 
@@ -14,7 +14,37 @@ console.log(a, b);
 const c = 5;
 //c = 50;
 
-console.log(c);
+console.log(c);*/
 
 // tipagem dentro de javaScript
+ /////////////////////////////////////////////////////// */
 
+let prova, trabalho, media;
+prova = 0
+trabalho = 2
+
+
+media = (prova + trabalho) / 2;
+
+
+ /*
+if (media < 6 && media >= 2){
+    console.log("talvez com sub");
+}
+    else if (media >= 6) {
+        console.log("Aprovado")
+    }
+    else(media < 6 )
+    console.log("Reprovado")
+*/ 
+
+/*
+if (media >=6 ){
+    console.log("aprovado")
+}
+else if (trabalho < 2){
+    console.log("reprovado")
+}
+else((trabalho+10)/2 >=6)
+console.log("talvez com a sub")
+*/
