6e2c6fe035bbb7de85c3ccb9a3947a3b8015ef1d
Kevyn Lima <kevynlimasilva1@gmail.com>
2022-07-12 22:08:47 -0300

    Atribuições

---
+++ fundamentos/atribuicao.js
@@ -0,0 +1,16 @@
+const a = 7
+let b = 3
+b += a // atribuição aditiva, nao é substituição mas sim acressentando o valor de a para b o que equivale b = b + a
+console.log(b) 
+
+b -= 4 // b = b - 4
+console.log(b)
+
+b *=2 // b = b * 2
+console.log(b)
+
+b /= 2 // b = b / 2 
+console.log(b)
+
+b %= 2 // b = b % 2
+console.log(b)
