c95705f4b3ae3fe267d2a3ece6f6ced2d663950a
Kevyn Lima <kevynlimasilva1@gmail.com>
2022-07-14 09:23:57 -0300

    If Else

---
+++ controle/if.js
@@ -0,0 +1,22 @@
+function soBoaNoticia(nota){
+    if (nota >= 7) {
+        console.log("Aprovado com " + nota)
+    }
+}
+
+soBoaNoticia(8.1)
+soBoaNoticia(6.1)
+
+function seForverdadeEuFalo(valor){
+    if(valor){
+        console.log('É verdade...' + valor)
+    }
+}
+seForverdadeEuFalo()
+seForverdadeEuFalo(null)
+seForverdadeEuFalo(undefined)
+seForverdadeEuFalo(NaN)
+seForverdadeEuFalo('')
+seForverdadeEuFalo(0)
+seForverdadeEuFalo(-1)
+seForverdadeEuFalo(' ')
