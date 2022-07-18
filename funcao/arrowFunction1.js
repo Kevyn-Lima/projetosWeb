7719c9d00d29a9c455128a665352b8f4d14cf457
Kevyn Lima <kevynlimasilva1@gmail.com>
2022-07-18 08:15:41 -0300

    Arrow Function

---
+++ funcao/arrowFunction1.js
@@ -0,0 +1,17 @@
+let Dobro = function a() {
+    return 2 *a
+}
+
+Dobro = (a) => {return 2 * a}
+
+Dobro = a => 2 * a // Return implicito
+
+console.log(Dobro(Math.PI))
+
+let ola = function(){
+    return 'Ola'
+} 
+
+ola = () => 'ola' // funções anonimas, voce pode colocar os parenteses sem paramentros
+ola = _=> 'Olá'// Ou pode colocar "_" porém, esse é um parametro, o que nao pode é deixar a Arrow function, sem paramentros, ou chamada da função "()"
+console.log(ola())
