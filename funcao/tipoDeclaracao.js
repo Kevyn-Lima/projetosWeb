04f65a0a4234a6e298cea2b8ae9959a3ad1e93e7
Kevyn Lima <kevynlimasilva1@gmail.com>
2022-07-20 07:33:54 -0300

    Declaração

---
+++ funcao/tipoDeclaracao.js
@@ -0,0 +1,17 @@
+// function declaration
+
+function soma(x, y) {
+    return x + y
+}
+
+// function expression
+// só consigo chamar a "sub" após a declaração
+const sub = function (x, y) {
+    return x - y
+}
+
+//named function expression
+const mult = function mult(x, y){
+    return x * y
+}
+
