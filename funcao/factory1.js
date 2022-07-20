0004c5b613b8485e0a1f2ea9cea678e9b97fab6e
Kevyn Lima <kevynlimasilva1@gmail.com>
2022-07-20 07:58:23 -0300

    Factory

---
+++ funcao/factory1.js
@@ -0,0 +1,11 @@
+// Factory simples
+// Factory: Função que retorna um ojeto = fabricar uma instancia de um objeto
+function criarPessoa(Pessoa){
+    return{
+        nome: 'Ana',
+        sobrenome: 'Alves'
+    }
+}
+console.log(criarPessoa())
+
+
