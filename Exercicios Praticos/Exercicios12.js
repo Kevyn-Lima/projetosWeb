30b495c497dcc0b4ba81aae93a17f75abad292d4
Kevyn Lima <kevynlimasilva1@gmail.com>
2022-11-16 19:39:34 -0300

    Exercicios

---
+++ Exercicios Praticos/Exercicios12.js
@@ -0,0 +1,11 @@
+function filtraNumeros(array){
+    const resultado = []
+    for(let item of resultado){
+        if(typeof item === "number"){
+            verifica.push(item)
+        }
+        return resultado
+    }
+} 
+
+console.log(filtraNumeros([0,2,1,4,5,'kevyn','lucas', 12,34,55]))
